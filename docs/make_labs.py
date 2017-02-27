8889e5f8a0bd2f6f9b9b728d261453749f1f7abc
jkerfs <kerfsj@gmail.com>
2017-02-26 17:39:02 -0800

    fixed template path for windows

--- docs/make_labs.py
+++ docs/make_labs.py
@@ -9,8 +9,10 @@ build_dir = os.path.join(cwd, "build")
 if not os.path.exists(build_dir):
     os.makedirs(build_dir)
 
-os.system("pandoc -V geometry:margin=1in " + os.path.join(cwd, "Syllabus.md") + " -s -o " + os.path.join(build_dir, "Syllabus.pdf") + " --template='support/template.tex'")
-os.system("pandoc " + os.path.join(cwd, "Syllabus.md") + " -s -o " + os.path.join(build_dir, "Syllabus.docx") + " --template='support/template.tex'")
+template_path = join('support', 'template.tex')
+
+os.system("pandoc -V geometry:margin=1in " + os.path.join(cwd, "Syllabus.md") + " -s -o " + os.path.join(build_dir, "Syllabus.pdf") + " --template='" + template_path + "'")
+os.system("pandoc " + os.path.join(cwd, "Syllabus.md") + " -s -o " + os.path.join(build_dir, "Syllabus.docx") + " --template='" + template_path + "'")
 
 projects_build_dir = os.path.join(build_dir, 'projects')
 if not os.path.exists(os.path.join(build_dir, 'projects')):
@@ -18,8 +20,8 @@ if not os.path.exists(os.path.join(build_dir, 'projects')):
 
 projects = [p for p in listdir(os.path.join(cwd, 'projects')) if '.md' in p]
 for p in projects:
-    os.system("pandoc -V geometry:margin=1in " + os.path.join(cwd, 'projects', p) + " -s -o " + os.path.join(projects_build_dir, p.replace(".md",".pdf")) + " --template='support/template.tex'")
-    os.system("pandoc " + os.path.join(cwd, 'projects', p) + " -s -o " + os.path.join(projects_build_dir, p.replace(".md",".docx")) + " --template='support/template.tex'")
+    os.system("pandoc -V geometry:margin=1in " + os.path.join(cwd, 'projects', p) + " -s -o " + os.path.join(projects_build_dir, p.replace(".md",".pdf")) + " --template='" + template_path + "'")
+    os.system("pandoc " + os.path.join(cwd, 'projects', p) + " -s -o " + os.path.join(projects_build_dir, p.replace(".md",".docx")) + " --template='" + template_path + "'")
 
 
 modules = [m for m in listdir(cwd) if "module" in m]
@@ -36,8 +38,8 @@ for m in modules:
 
         qfiles = [q for q in listdir(os.path.join(cwd, m, "questions"))]
         for q in qfiles:
-            os.system("pandoc -V geometry:margin=1in -f markdown-markdown_in_html_blocks " + os.path.join(cwd, m, "questions", q) + " -s -o " + os.path.join(question_build_dir, q.replace(".md",".pdf")) + " --template='support/template.tex' --from markdown+markdown_in_html_blocks")
-            os.system("pandoc -f markdown-markdown_in_html_blocks " + os.path.join(cwd, m, "questions", q) + " -s -o " + os.path.join(question_build_dir, q.replace(".md",".docx")) + " --template='support/template.tex' --from markdown+markdown_in_html_blocks")
+            os.system("pandoc -V geometry:margin=1in -f markdown-markdown_in_html_blocks " + os.path.join(cwd, m, "questions", q) + " -s -o " + os.path.join(question_build_dir, q.replace(".md",".pdf")) + " --template='" + template_path + "' --from markdown+markdown_in_html_blocks")
+            os.system("pandoc -f markdown-markdown_in_html_blocks " + os.path.join(cwd, m, "questions", q) + " -s -o " + os.path.join(question_build_dir, q.replace(".md",".docx")) + " --template='" + template_path + "' --from markdown+markdown_in_html_blocks")
 
     # Build Lab Documentation and Code zips
     labs = [l for l in listdir(os.path.join(cwd, m)) if "lab" in l]
@@ -49,8 +51,8 @@ for m in modules:
         # Compile markdown files to pdfs
         md_files = [md for md in listdir(os.path.join(cwd, m, l)) if ".md" in md]
         for md in md_files:
-            os.system("pandoc -V geometry:margin=1in " + os.path.join(cwd, m, l, md) + " -s -o " + os.path.join(lab_build_dir, md.replace(".md",".pdf")) + " --template='support/template.tex'")
-            os.system("pandoc " + os.path.join(cwd, m, l, md) + " -s -o " + os.path.join(lab_build_dir, md.replace(".md",".docx")) + " --template='support/template.tex'")
+            os.system("pandoc -V geometry:margin=1in " + os.path.join(cwd, m, l, md) + " -s -o " + os.path.join(lab_build_dir, md.replace(".md",".pdf")) + " --template='" + template_path + "'")
+            os.system("pandoc " + os.path.join(cwd, m, l, md) + " -s -o " + os.path.join(lab_build_dir, md.replace(".md",".docx")) + " --template='" + template_path + "'")
 
         # Zip the source directory
         if os.path.exists(os.path.join(cwd, m, l, "code")):
